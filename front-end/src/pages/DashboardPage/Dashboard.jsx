d98ea70c217ef9bf2114406ccdb23d1cba71f4be
Swostika Lama <Swostika.Lama@metropolia.fi>
2025-11-19 23:03:08 +0200

    dashboardpage is added

---
+++ front-end/src/pages/DashboardPage/Dashboard.jsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <div className="text-center mt-20 text-xl text-gray-700">
+      Welcome to your Dashboard!
+    </div>
+  );
+};
+
+export default Dashboard;
