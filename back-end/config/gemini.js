b465f96b095846b483476e910256aeb0e7bcc9dd
Puntawat Subhamani <puntawatsub@gmail.com>
2025-12-10 12:44:06 +0200

    change model to 1.5 flash

--- back-end/config/gemini.js
+++ back-end/config/gemini.js
@@ -6,7 +6,7 @@ const { GoogleGenAI } = require("@google/genai");
 // "models/gemini-2.5-flash", "models/gemini-2.0-flash","models/gemini-2.5-flash-lite","models/gemini-2.5-flash-image-preview", "models/gemini-1.5-flash"
 
 // GoogleGenerativeAI setup
-const MODEL_NAME = "models/gemini-2.5-flash-lite";
+const MODEL_NAME = "models/gemini-1.5-flash";
 
 const model = async (prompt) => {
   const contents = [{ role: "user", parts: [{ text: prompt }] }];
